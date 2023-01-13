0b598c606b2d065aec824bb16f9948a3982789c2
LucasDanieel <lucasdaniel545@gmail.com>
2023-01-13 07:28:16 -0400

    Initial

---
+++ src/templates/Home/index.tsx
@@ -0,0 +1,6 @@
+import { CloneMe } from '../../components/CloneMe';
+import * as Styled from './styled'
+
+export function Home() {
+  return (<Styled.Wrapper><CloneMe title='Clone-me' /></Styled.Wrapper>);
+}
